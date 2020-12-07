94a42c32a7dd272e74653ac433fb74f724c5da24
Léandre Le Polles--Potin <leandre.lepolles@free.fr>
2020-12-07 12:40:37 +0100

    Basic movement down

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,27 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class MainController : MonoBehaviour
 {
     [SerializeField] protected float maxSpeed = 10f;
     [SerializeField] protected float accelerationTime = 0.3f;
-    [SerializeField] protected float fieldWidth = 10f;
+    [SerializeField] protected float fieldWidth = 5f;
 
-    private Rigidbody rigidbody;
-    private float currentSpeed;
+    private Vector2 currentSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
-        currentSpeed = 0;
+        currentSpeed = Vector2.zero;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        var inputMovement = Input.GetAxis("Horizontal");
+        var targetSpeed = Vector2.right * inputMovement * maxSpeed;
+        currentSpeed = Vector2.MoveTowards(currentSpeed, targetSpeed, (maxSpeed / accelerationTime) * Time.fixedDeltaTime);
+    }
+
+    private void Update() {
+        var newPosition = transform.position + new Vector3(currentSpeed.x, 0, currentSpeed.y) * Time.deltaTime;
+        if( newPosition.x >= fieldWidth / 2) {
+            newPosition.x = fieldWidth / 2;
+            currentSpeed.x = 0;
+        }
+        else if ( newPosition.x <= -fieldWidth / 2 ) {
+            newPosition.x = -fieldWidth / 2;
+            currentSpeed.x = 0;
+        }
+        transform.position = newPosition;
+    }
+    
+    private void OnDrawGizmos() {
+        #if UNITY_EDITOR
+        // if( rigidbody != null )
+            Handles.Label(transform.position, 
+                "Input movement: " + Input.GetAxis("Horizontal") + "\n" +
+                "Velocity: " + currentSpeed.x
+            );
+        #endif
     }
 }
