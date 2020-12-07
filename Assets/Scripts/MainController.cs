19b6ec3540f4d414280dd8200e3d810a32aa87bd
Léandre Le Polles--Potin <leandre.lepolles@free.fr>
2020-12-07 12:51:08 +0100

    Better movement + Gizmos

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -6,15 +6,16 @@ using UnityEngine;
 public class MainController : MonoBehaviour
 {
     [SerializeField] protected float maxSpeed = 10f;
-    [SerializeField] protected float accelerationTime = 0.3f;
+    [SerializeField, Min(0)] protected float accelerationTime = 0.3f;
     [SerializeField] protected float fieldWidth = 5f;
 
-    private Vector2 currentSpeed;
+    private Vector2 currentSpeed = Vector2.zero;
+    private Vector3 MovementSpeed => new Vector3(currentSpeed.x, 0, currentSpeed.y);
 
     // Start is called before the first frame update
     void Start()
     {
-        currentSpeed = Vector2.zero;
+        // currentSpeed
     }
 
     // Update is called once per frame
@@ -22,19 +23,20 @@ public class MainController : MonoBehaviour
     {
         var inputMovement = Input.GetAxis("Horizontal");
         var targetSpeed = Vector2.right * inputMovement * maxSpeed;
-        currentSpeed = Vector2.MoveTowards(currentSpeed, targetSpeed, (maxSpeed / accelerationTime) * Time.fixedDeltaTime);
+
+        if( Mathf.Sign( currentSpeed.x ) != Mathf.Sign( targetSpeed.x ) )
+            currentSpeed.x = 0;
+
+        if( accelerationTime > 0 )
+            currentSpeed = Vector2.MoveTowards(currentSpeed, targetSpeed, (maxSpeed / accelerationTime) * Time.fixedDeltaTime);
+        else
+            currentSpeed = targetSpeed;
     }
 
     private void Update() {
-        var newPosition = transform.position + new Vector3(currentSpeed.x, 0, currentSpeed.y) * Time.deltaTime;
-        if( newPosition.x >= fieldWidth / 2) {
-            newPosition.x = fieldWidth / 2;
-            currentSpeed.x = 0;
-        }
-        else if ( newPosition.x <= -fieldWidth / 2 ) {
-            newPosition.x = -fieldWidth / 2;
-            currentSpeed.x = 0;
-        }
+        var newPosition = transform.position + MovementSpeed * Time.deltaTime;
+        newPosition.x = Mathf.Clamp( newPosition.x, -fieldWidth/2, fieldWidth/2);
+
         transform.position = newPosition;
     }
     
@@ -46,5 +48,12 @@ public class MainController : MonoBehaviour
                 "Velocity: " + currentSpeed.x
             );
         #endif
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine( new Vector3(-fieldWidth/2, 1, 10), new Vector3(-fieldWidth/2, 1, -10) );
+        Gizmos.DrawLine( new Vector3(fieldWidth/2, 1, 10), new Vector3(fieldWidth/2, 1, -10) );
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine( transform.position, transform.position + MovementSpeed * 0.1f);
     }
 }
