312d4fe943a228f7729901f4143dcc617be57bef
Léandre Le Polles--Potin <leandre.lepolles@free.fr>
2020-12-07 10:53:37 +0100

    Setup Unity project

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MainController : MonoBehaviour
+{
+    [SerializeField] protected float maxSpeed = 10f;
+    [SerializeField] protected float accelerationTime = 0.3f;
+    [SerializeField] protected float fieldWidth = 10f;
+
+    private Rigidbody rigidbody;
+    private float currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        currentSpeed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
