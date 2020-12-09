afff4a83e9801053707c42f812be267eac2bd94d
Léandre Le Polles--Potin <leandre.lepolles@free.fr>
2020-12-09 14:32:38 +0100

    Cel shading shader done

---
+++ Assets/Materials/CelShadingScript.hlsl
@@ -0,0 +1,20 @@
+﻿void URPLightningFunction_float (float3 ObjPos, out float3 Color, out float3 Direction, out float ShadowAttenuation)
+{
+   #ifdef LIGHTWEIGHT_LIGHTING_INCLUDED
+   
+      //Actual light data from the pipeline
+      Light light = GetMainLight(GetShadowCoord(GetVertexPositionInputs(ObjPos)));
+      Color = light.color;
+      Direction = light.direction;
+      ShadowAttenuation = light.shadowAttenuation;
+      
+   #else
+   
+      //Hardcoded data, used for the preview shader inside the graph
+      //where light functions are not available
+      Color = float3(1, 1, 1);
+      Direction = float3(-0.5, 0.5, -0.5);
+      ShadowAttenuation = 0.4;
+      
+   #endif
+}
