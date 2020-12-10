2baef8ff9ac75fdbfe8a53ab3236ee64172e1bb9
Léandre Le Polles--Potin <leandre.lepolles@free.fr>
2020-12-10 19:31:43 +0100

    Main Menu animation

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.SceneManagement;
+
+public enum MenuState {
+    MainMenu,
+    Credits,
+    Tutorial,
+    Starting,
+    Exiting
+}
+
+public class MainMenu : MonoBehaviour
+{
+    public CinemachinePath mainToCredits;
+    public CinemachinePath creditsToMain;
+
+    public CinemachinePath mainToTuto;
+    public CinemachinePath tutoToMain;
+
+    public CinemachinePath mainToStart;
+    public CinemachinePath mainToQuit;
+
+    public CinemachineDollyCart cameraDollyCart;
+
+    private MenuState currentState;
+
+    private Animator animator;
+    private void Start() {
+        currentState = MenuState.MainMenu;
+        animator = GetComponent<Animator>();
+    }
+
+    public void StartGame() {
+        currentState = MenuState.Starting;
+        AnimationDoneCallback();
+        // animator.Play("gotoStart");
+    }
+
+    public void GotoCredits() {
+        currentState = MenuState.Credits;
+        cameraDollyCart.m_Path = mainToCredits;
+        animator.Play("gotoCredits");
+    }
+
+    public void GotoTuto() {
+        currentState = MenuState.Tutorial;
+        animator.Play("gotoTuto");
+    }
+
+    public void BackToMain() {
+        switch(currentState) {
+            case MenuState.Credits:
+                cameraDollyCart.m_Path = creditsToMain;
+                animator.Play("creditsToMain");
+                break;
+
+            case MenuState.Tutorial:
+                cameraDollyCart.m_Path = tutoToMain;
+                animator.Play("tutoToMain");
+                break;
+
+            default:
+                break;
+        }
+
+        currentState = MenuState.MainMenu;
+    }
+
+    public void Quit() {
+        currentState = MenuState.Starting;
+        AnimationDoneCallback();
+        // animator.Play("gotoQuit");
+    }
+
+    public void AnimationDoneCallback() {
+        switch(currentState) {
+            case MenuState.Starting:
+                SceneManager.LoadScene(1);
+                break;
+            case MenuState.Exiting:
+                Application.Quit();
+                break;
+            case MenuState.MainMenu:
+                animator.Play("default");
+                cameraDollyCart.m_Position = 0;
+                cameraDollyCart.m_Path = mainToCredits;
+                break;
+        }
+    }
+}
