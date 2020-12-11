b7ae13f1a27722113fc2cb8c3e64d101ca69950c
Léandre Le Polles--Potin <leandre.lepolles@free.fr>
2020-12-11 15:52:40 +0100

    Coach prefabs

--- Assets/Scripts/controllers/CoachController.cs
+++ Assets/Scripts/controllers/CoachController.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class CoachController : MonoBehaviour
 {
     public QuaterbackController quaterback;
+    public Animator coachAnimator;
+    public float rotationSpeed = 90f;
     protected Quaternion targetRotation = Quaternion.identity;
+
+    private int direction = 0;
     
     private void Update() {
         transform.position = quaterback.transform.position;
         
-        if( Input.GetButtonDown("RotateRight") )
-            transform.rotation *= Quaternion.Euler(0, 0, 90);
+        if( Input.GetButtonDown("RotateRight") ) direction ++;
+        if( Input.GetButtonUp("RotateLeft") ) direction ++;
+            // transform.rotation *= Quaternion.Euler(0, 0, 90);
         
-        
-        if( Input.GetButtonDown("RotateLeft") )
-            transform.rotation *= Quaternion.Euler(0, 0, -90);
+        if( Input.GetButtonDown("RotateLeft") ) direction --;
+        if( Input.GetButtonUp("RotateRight") ) direction --;
+
+        coachAnimator.SetInteger("direction", direction);
+            
+        transform.rotation *= Quaternion.Euler(0, 0, rotationSpeed * direction * Time.deltaTime);
     }
 }
