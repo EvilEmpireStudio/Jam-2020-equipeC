f045e9cb72df00d2d9c433044845ac766bec493b
Léandre Le Polles--Potin <leandre.lepolles@free.fr>
2020-12-11 12:36:34 +0100

    Dynamic culling to try and reduce the lag

---
+++ Assets/DynamicActivating.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicActivating : MonoBehaviour
+{
+    public QuaterbackController quaterbackController;
+    public float activatingDistance = 200f;
+    public float deactivatingDistance = 200f;
+
+    private void FixedUpdate() {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+
+            if( child.position.y < quaterbackController.transform.position.y - deactivatingDistance ) {
+                child.gameObject.SetActive(false);
+                Destroy(child.gameObject);
+            }
+            else if( child.position.y < quaterbackController.transform.position.y + activatingDistance )
+                child.gameObject.SetActive(true);
+        }
+    }
+}
