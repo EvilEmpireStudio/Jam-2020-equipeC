32d814259e17b52bd135618eecc2a3f1f34be110
Léandre Le Polles--Potin <leandre.lepolles@free.fr>
2020-12-11 17:06:42 +0100

    Optimization

---
+++ Assets/Activator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if( other.GetComponent<QuaterbackController>() != null )
+            for (int i = 0; i < transform.childCount; i++)
+                transform.GetChild(i).gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if( other.GetComponent<QuaterbackController>() != null )
+            for (int i = 0; i < transform.childCount; i++)
+                transform.GetChild(i).gameObject.SetActive(false);
+        // if( sectionToManage != null && other.GetComponent<QuaterbackController>() != null )
+        //     sectionToManage.gameObject.SetActive(false);
+    }
+}
