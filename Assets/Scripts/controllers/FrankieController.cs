9177b50f577caad96d24c9d0d1e1db3b68ad4ed4
Léandre Le Polles--Potin <leandre.lepolles@free.fr>
2020-12-11 14:53:00 +0100

    Frankies, teams and anims

--- Assets/Scripts/controllers/FrankieController.cs
+++ Assets/Scripts/controllers/FrankieController.cs
@@ -10,6 +10,7 @@ public class FrankieController : MonoBehaviour
     public QuaterbackController quaterback = default;
     public ColliderList defendZone = default;
     public ParticleSystem downParticles = default;
+    public Animator animator = default;
 
     public float speed = 10f;
     [Range(0,1)] public float followStrengh = 0.75f;
@@ -36,6 +37,8 @@ public class FrankieController : MonoBehaviour
     }
 
     private void Down() {
+        animator.Play("death");
+
         isDown = true;
         if( downParticles != null )
             Instantiate(downParticles, transform.position, transform.rotation).Play();
@@ -45,10 +48,13 @@ public class FrankieController : MonoBehaviour
     internal void Rise()
     {
         isDown = false;
+        animator.Play("run");
     }
 
     private void DestroyObstacles()
     {
+        animator.Play("kick");
+
         List<Parryable> obstacles = defendZone.colliders
             .FindAll( collider => collider.GetComponent<Parryable>() != null )
             .ConvertAll<Parryable>( collider => collider.GetComponent<Parryable>() );
